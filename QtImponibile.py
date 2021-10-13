2e96c9ef49250d6b804ff3cc49ce7c878e8c612f
braucci <raucci@gmail.com>
2021-10-13 14:27:57 +0200

    Add files via upload

--- QtImponibile.py
+++ QtImponibile.py
@@ -7,15 +7,28 @@ class Ui(QWidget):
         super().__init__()
         uic.loadUi('imponibile.ui', self)
         self.setFixedSize(399, 177)
+    
 
     def pB_valutaClick(self):
+
+        def imponibile(n1,n2):
+            n3 = n1*100/(n2+100)
+            return n3
+        
+        def iva(n1, n3):
+            n4 = n1-n3
+            return n4
+
+        
         try:
             n1 = float(self.lE_totFattura.text())
             n2 = float(self.lE_aliquota.text())
-            n3 = n1*100/(n2+100)
-            n4 = n1-n3
-            self.lE_imponibile.setText(str(format(n3,'>12.2f')))
-            self.lE_iva.setText(str(format(n4,'>12.2f')))
+            
+            impvalue = imponibile(n1,n2)
+            ivavalue = iva(n1,impvalue)
+
+            self.lE_imponibile.setText(str(format(impvalue,'>12.2f')))
+            self.lE_iva.setText(str(format(ivavalue,'>12.2f')))
             self.l_errore.setText('')
         except:
             self.l_errore.setText('Valore numerico non accettato')
